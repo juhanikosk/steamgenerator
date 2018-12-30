dbcdcc1baa86d4227f2d858cfe7f0e3eddfca303
juhani.koskinen1@edu.turkuamk.fi <juhanikos@gmail.com>
2018-12-30 21:26:48 +0200

    Updates

---
+++ precache-manifest.c9520a88bd5d8e34c83f5d5c7a62c445.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9772a1bcc77af0c2b1e1cd229c9f16df",
+    "url": "/steamgenerator/static/media/guide.9772a1bc.PNG"
+  },
+  {
+    "revision": "0a195977096332d4303018b6b15e53f2",
+    "url": "/steamgenerator/static/media/Thasadith-Bold.0a195977.ttf"
+  },
+  {
+    "revision": "767b692abc18c7ea49cb",
+    "url": "/steamgenerator/static/js/runtime~main.767b692a.js"
+  },
+  {
+    "revision": "9927e778569913f0381b",
+    "url": "/steamgenerator/static/js/main.9927e778.chunk.js"
+  },
+  {
+    "revision": "d4c7a4a955215d5126c0",
+    "url": "/steamgenerator/static/js/1.d4c7a4a9.chunk.js"
+  },
+  {
+    "revision": "9927e778569913f0381b",
+    "url": "/steamgenerator/static/css/main.0cb0ae0e.chunk.css"
+  },
+  {
+    "revision": "656016dd03d0b65ea2e812d3a3da82ee",
+    "url": "/steamgenerator/index.html"
+  }
+];
