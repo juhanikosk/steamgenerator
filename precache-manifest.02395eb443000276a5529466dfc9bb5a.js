43845d6f5c539b6bd29b6f31fc7819021c259567
juhani.koskinen1@edu.turkuamk.fi <juhanikos@gmail.com>
2018-12-31 14:33:18 +0200

    Updates

---
+++ precache-manifest.02395eb443000276a5529466dfc9bb5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9772a1bcc77af0c2b1e1cd229c9f16df",
+    "url": "/steamgenerator/static/media/guide.9772a1bc.PNG"
+  },
+  {
+    "revision": "767b692abc18c7ea49cb",
+    "url": "/steamgenerator/static/js/runtime~main.767b692a.js"
+  },
+  {
+    "revision": "2880fa67cbe57e8dbaeb",
+    "url": "/steamgenerator/static/js/main.2880fa67.chunk.js"
+  },
+  {
+    "revision": "2ab1be6d4715783e6812",
+    "url": "/steamgenerator/static/js/1.2ab1be6d.chunk.js"
+  },
+  {
+    "revision": "2880fa67cbe57e8dbaeb",
+    "url": "/steamgenerator/static/css/main.bb549637.chunk.css"
+  },
+  {
+    "revision": "5ab7b17b77175f5987a7f12993229cd6",
+    "url": "/steamgenerator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/steamgenerator/precache-manifest.0f30080486e5cb521f4ad5e3fee8d1d2.js"
+  "/steamgenerator/precache-manifest.02395eb443000276a5529466dfc9bb5a.js"
 );
 
 workbox.clientsClaim();
